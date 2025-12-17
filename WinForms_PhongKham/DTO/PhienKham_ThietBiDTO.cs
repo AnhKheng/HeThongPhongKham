220519b5b4981b07b96bee76f657b5c96782f6df
Dow <thaihocng204@gmail.com>
2025-12-17 23:23:22 +0700

    done DTO

---
+++ WinForms_PhongKham/DTO/PhienKham_ThietBiDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinForms_PhongKham.DTO
+{
+	public class PhienKham_ThietBiDTO
+	{
+		public int PhienKham_ThietBiID { get; set; }
+		public int PhienKhamID { get; set; }
+		public int ThietBiID { get; set; }
+		public int SoLuong { get; set; }
+		public string GhiChu { get; set; }
+	}
+}
---
+++ WinForms_PhongKham/DTO/ThongTinCaNhanDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinForms_PhongKham.DTO
+{
+    public class ThongTinCaNhanDTO
+	{
+		public int ThongTinID { get; set; }
+		public int? TaiKhoanID { get; set; }
+		public string HoTen { get; set; }
+		public DateTime? NgaySinh { get; set; }
+		public string GioiTinh { get; set; }
+		public string SDT { get; set; }
+		public string EmailLienHe { get; set; }
+		public string DiaChi { get; set; }
+		public string Avatar { get; set; }
+		public string Loai { get; set; }
+		public DateTime NgayTao { get; set; }
+		public DateTime NgayCapNhat { get; set; }
+	}
+}
